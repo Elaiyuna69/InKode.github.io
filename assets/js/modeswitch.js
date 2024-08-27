97b61a3a6da1a8283281c8c094e3c0863e4f4c24
Elaiyuna69 <159261204+Elaiyuna69@users.noreply.github.com>
2024-08-27 22:19:09 +0800

    Fix : Continuity, variable

---
+++ assets/js/modeswitch.js
@@ -0,0 +1,22 @@
+let isDarkMode = true;
+
+function switchMode() {
+  isDarkMode = !isDarkMode;
+  const newGradient1 = isDarkMode
+    ? "rgba(2, 4, 21, 1)"
+    : "rgba(255, 255, 255, 1)";
+  const newGradient2 = isDarkMode
+    ? "rgba(22, 30, 84, 1)"
+    : "rgba(0, 88, 156, 1)";
+  const newNeutral = isDarkMode
+    ? "#fff"
+    : "#000";
+    const newNav = isDarkMode
+    ? "#000818c8"
+    : "#d6e4ffc8";
+  document.documentElement.style.setProperty("--gradient-1", newGradient1);
+  document.documentElement.style.setProperty("--gradient-2", newGradient2);
+  document.documentElement.style.setProperty("--neutral", newNeutral);
+  document.documentElement.style.setProperty("--nav-background", newNav);
+}
+
